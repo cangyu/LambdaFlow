8b0c6105142b10000334079c9de2a22c7976f342
Yu Cang <yhcy1993@gmail.com>
2018-07-03 00:00:08 +0800

    fix misunderstanding

--- VV570/LAB2/Task3_a.m
+++ VV570/LAB2/Task3_a.m
@@ -6,9 +6,9 @@ traperror = zeros(1, N);
 simerror = zeros(1, N);
 for i = 1:N
     fsym = symfun(i*x^(i-1), x);
-    miderror(i) = midpointquad(fsym,0,1,i)-1;
-    traperror(i) = trapezoidalquad(fsym, 0, 1, i)-1;
-    simerror(i) = simpsonquad(fsym, 0, 1, i)-1;
+    miderror(i) = midpointquad(fsym,0,1,1)-1;
+    traperror(i) = trapezoidalquad(fsym, 0, 1, 1)-1;
+    simerror(i) = simpsonquad(fsym, 0, 1, 1)-1;
 end
 format long
 monomial = {'$1$';'$2x$';'$3x^2$';'$4x^3$';'$5x^4$'};
