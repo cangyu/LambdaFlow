2bc497029caf76424aeec4dd9e8b43f58e3593e0
Yu Cang <yhcy1993@gmail.com>
2018-07-02 11:03:41 +0800

    Task2 done

--- VV570/LAB2/Task2_d.m
+++ VV570/LAB2/Task2_d.m
@@ -5,3 +5,16 @@ exactf = int(fsym, -1, 1);
 format longE
 eval(exactf)
 format short
+
+% n = 20;
+% [xval, w] = lgwt(n, -1, 1);
+% yval = fano(xval);
+% quad = sum(yval.*w);
+% err = eval(quad - exactf);
+
+n = 20;
+[xval, w] = fclencurt(n+2, -1, 1);
+yval = fano(xval);
+quad = sum(yval.*w)
+err = eval(quad - exactf)
+
