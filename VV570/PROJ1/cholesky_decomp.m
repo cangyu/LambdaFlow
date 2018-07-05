33e1cdb96931faba477545a5c22615eddbc1bd91
Yu Cang <yhcy1993@gmail.com>
2018-07-05 14:00:41 +0800

    basical cholesky decomp

--- VV570/PROJ1/cholesky_decomp.m
+++ VV570/PROJ1/cholesky_decomp.m
@@ -1,5 +1,22 @@
 function [L] = cholesky_decomp(A)
     [n, ~] = size(A);
+    L = zeros(n, n);
     
+    for j = 1:n
+        for i = j:n
+            s=0;
+            k=1;
+            while(k<j)
+                s = s+L(i,k)*L(j,k);
+                k = k+1;
+            end
+            
+            if(i==j)
+                L(i, j) = sqrt(A(i, j) - s);
+            else
+                L(i, j) = (A(i,j)-s)/L(j, j);
+            end
+        end
+    end
 end
 
