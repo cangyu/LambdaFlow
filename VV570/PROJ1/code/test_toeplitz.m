5121400d255b84835aa28c2fd32bd00499c6a718
Yu Cang <yhcy1993@gmail.com>
2018-07-09 14:12:47 +0800

    test revised

--- VV570/PROJ1/code/test_toeplitz.m
+++ VV570/PROJ1/code/test_toeplitz.m
@@ -1,6 +1,11 @@
-p = 10;
-r = rand(1, p+1)
-T = toeplitz(r(:,1:p))
-b = r(:,2:p+1)
-a1 = inv(T)*b'
-a2 = solve_toeplitz(r)'
+p = 1000;
+r = rand(1, p+1);
+T = toeplitz(r(:,1:p));
+b = r(:,2:p+1);
+tic
+    a1 = inv(T)*b';
+toc
+
+tic
+    a2 = solve_toeplitz(r)';
+toc
