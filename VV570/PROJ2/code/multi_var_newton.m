b4cedf54e10bcdb71bae0e4fbec15df22f8cfb90
Yu Cang <yhcy1993@gmail.com>
2018-07-29 14:01:13 +0800

    task1 code basically ok

--- VV570/PROJ2/code/multi_var_newton.m
+++ VV570/PROJ2/code/multi_var_newton.m
@@ -1,16 +1,16 @@
-function [x] = multi_var_newton(F, dF, x0, tol, custom_norm)
+function [x] = multi_var_newton(f, Jf, x0, x_tol, y_tol, custom_norm)
     x = x0;
-    fx = F(x);
-    dfx = dF(x);
-    delta_x = linsolve(dfx, -fx);
-    x = x + delta_x;
-    err = custom_norm(delta_x);
-    while(err > tol)
-        fx = F(x);
-        dfx = dF(x);
-        delta_x = linsolve(dfx, -fx);
-        x = x + delta_x;
-        err = custom_norm(delta_x);
+    y_err = custom_norm(f(x));
+    if(y_err <= y_tol)
+        return;
+    end
+
+    x_err = 0;
+    while(x_err > x_tol || y_err > y_tol)
+        dx = linsolve(Jf(x), -f(x));
+        x_err = custom_norm(dx);
+        x = x + dx;
+        y_err = custom_norm(f(x));
     end
 end
 
