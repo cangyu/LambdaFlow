0b0d4b2856fb220cd79ac318b1f0a7f550c9db3f
Yu Cang <yhcy1993@gmail.com>
2018-08-02 15:27:16 +0800

    task3 ok

--- VV570/PROJ2/code/task3.m
+++ VV570/PROJ2/code/task3.m
@@ -4,9 +4,8 @@ alpha = 0.1;
 gamma = 0.2;
 phi = @(t, x) [gamma-alpha*x(1)^2*x(2); alpha*x(1)^2*x(2)-x(2)/(1+x(2))];
 x0 = [1;1];
-
 %--------------------------------------------------------------------------
-disp('Part (a) -- x(t) and n(t) are calculated with ode45');
+disp('Part(a) -- x(t) and n(t) are calculated with ode45');
 %--------------------------------------------------------------------------
 N = 501;
 a = 0;
@@ -18,7 +17,7 @@ y = deval(sol, t);
 n = y(1,:);
 x = y(2,:);
 %--------------------------------------------------------------------------
-disp('Part (b) -- alpha and gamma are approximated at each point using central difference');
+disp('Part(b) -- alpha and gamma are approximated at each point using central difference');
 %--------------------------------------------------------------------------
 alpha_sol1 = zeros(1, N);
 gamma_sol1 = zeros(1, N);
@@ -30,7 +29,7 @@ gamma_est1 = mean(gamma_sol1(2:N-1));
 fprintf('  estimated alpha: %f\n', alpha_est1);
 fprintf('  estimated gamma: %f\n', gamma_est1);
 %--------------------------------------------------------------------------
-disp('Part (c) -- noise signals are generated using normrnd');
+disp('Part(c) -- noise signals are generated using normrnd');
 %--------------------------------------------------------------------------
 sigma1 = sqrt(0.1);
 sigma2 = sqrt(0.001);
@@ -38,8 +37,32 @@ eta = normrnd(0, sigma1, 1, N);
 mu = normrnd(0, sigma2, 1, N);
 ns = n + eta;
 xs = x + mu;
+
+figure(1)
+subplot(2,1,1)
+plot(t, n)
+xlabel('t');
+ylabel('n(t)');
+title('original n(t)')
+subplot(2,1,2)
+plot(t, ns)
+xlabel('t');
+ylabel('n^*(t)');
+title('n(t) with noise');
+
+figure(2)
+subplot(2,1,1)
+plot(t, x)
+xlabel('t');
+ylabel('x(t)');
+title('original x(t)')
+subplot(2,1,2)
+plot(t, xs)
+xlabel('t');
+ylabel('x^*(t)');
+title('x(t) with noise');
 %--------------------------------------------------------------------------
-disp('Part (d) -- alpha and gamma are estimated utilizing the properies of nosie');
+disp('Part(d) -- alpha and gamma are estimated utilizing the properies of nosie');
 %--------------------------------------------------------------------------
 en = mean(ns);
 ex = mean(xs);
@@ -68,45 +91,62 @@ gamma_est2 = ensp + alpha_est2*en2x;
 fprintf('  estimated alpha: %f\n', alpha_est2);
 fprintf('  estimated gamma: %f\n', gamma_est2);
 %--------------------------------------------------------------------------
-disp('Part (e) -- alpha and gamma are estimated without utilizing the properies of nosie');
+disp('Part(e) -- alpha and gamma are estimated without utilizing the properies of nosie');
 %--------------------------------------------------------------------------
-alpha_sol2 = zeros(1, N);
-gamma_sol2 = zeros(1, N);
-for i = 2:N-1
-    [alpha_sol2(i), gamma_sol2(i)] = central_diff_param_solve(ns, xs, i, dt);
+n_trial = 200;
+alpha_est3 = zeros(1, n_trial);
+gamma_est3 = zeros(1, n_trial);
+for k = 1:n_trial
+    cns = n + normrnd(0, sigma1, 1, N);
+    cxs = x + normrnd(0, sigma2, 1, N);
+    cur_alpha_sol = zeros(1, N);
+    cur_gamma_sol = zeros(1, N);
+    for i = 2:N-1
+        [cur_alpha_sol(i), cur_gamma_sol(i)] = central_diff_param_solve(cns, cxs, i, dt);
+    end
+    alpha_est3(k) = mean(cur_alpha_sol(2:N-1));
+    gamma_est3(k) = mean(cur_gamma_sol(2:N-1));
 end
-alpha_star_est = mean(alpha_sol2(2:N-1));
-gamma_star_est = mean(gamma_sol2(2:N-1));
-fprintf('  estimated alpha: %f\n', alpha_star_est);
-fprintf('  estimated gamma: %f\n', gamma_star_est);
-
-%--------------------------------------------------------------------------
-disp('Plotting...');
-%--------------------------------------------------------------------------
-figure(1)
+figure(3)
 subplot(2,1,1)
-plot(t, n)
-xlabel('t');
-ylabel('n(t)');
-title('original n(t)')
+plot(alpha_est3)
+xlabel('Numerical Experiment Cnt')
+ylabel('\alpha')
+title('Coarse estimation of \alpha')
 subplot(2,1,2)
-plot(t, ns)
-xlabel('t');
-ylabel('n^*(t)');
-title('n(t) with noise');
+plot(gamma_est3)
+xlabel('Numerical Experiment Cnt')
+ylabel('\gamma')
+title('Coarse estimation of \gamma')
 
-figure(2)
+alpha_est3_mean = mean(alpha_est3);
+gamma_est3_mean = mean(gamma_est3);
+
+alpha_est3_err = zeros(1, n_trial);
+gamma_est3_err = zeros(1, n_trial);
+for k = 2:n_trial
+    alpha_est3_err(k) = abs((alpha_est3(k)-alpha_est3_mean)/alpha_est3_mean);
+    gamma_est3_err(k) = abs((gamma_est3(k)-gamma_est3_mean)/gamma_est3_mean);
+end
+figure(4)
 subplot(2,1,1)
-plot(t, x)
-xlabel('t');
-ylabel('x(t)');
-title('original x(t)')
+plot(alpha_est3_err);
+xlabel('Numerical Experiment Cnt')
+ylabel('Relative error of \alpha')
+title('Coarse estimation of \alpha')
 subplot(2,1,2)
-plot(t, xs)
-xlabel('t');
-ylabel('x^*(t)');
-title('x(t) with noise');
+plot(gamma_est3_err);
+xlabel('Numerical Experiment Cnt')
+ylabel('Relative error of \gamma')
+title('Coarse estimation of \gamma')
 
+gamma_est4 = ensp+exsp+exo1xs;
+alpha_est4 = (gamma_est4-ensp)/en2xs;
+fprintf('  estimated alpha: %f\n', alpha_est4);
+fprintf('  estimated gamma: %f\n', gamma_est4);
+%--------------------------------------------------------------------------
+disp('Done!');
+%--------------------------------------------------------------------------
 
 function [ak, gk] = central_diff_param_solve(n, x, i, dt)
     n0 = n(i-1); x0 = x(i-1);
