20f0e506a88e4b5189db681e37cf4b6fa9cc7854
Yu Cang <yhcy1993@gmail.com>
2018-11-01 21:25:20 +0800

    fix rho

--- VM432/HW3/Q2.m
+++ VM432/HW3/Q2.m
@@ -1,43 +1,42 @@
 clear all; close all; clc;
 
+Tu = 300;
 T = 1500;
 P = oneatm;
-fprintf("Flame Thickness Estimation at T=%d K, P=%d Pa\n", T, P);
+fprintf('Flame Thickness Estimation at T=%d K, P=%d Pa\n', T, P);
 
-fprintf("===============Mehtane-Oxygen================\n");
+fprintf('===============Mehtane-Oxygen================\n');
 gas = GRI30('Mix');
 gascomp = 'CH4:1, O2:2';
-set(gas,'T', T, 'P', P, 'X', gascomp);
-
-lambda = thermalConductivity(gas);
-fprintf("\tThermalConductivity: %f W/(mK)\n", lambda);
+set(gas,'T', Tu, 'P', P, 'X', gascomp);
 rho = density(gas);
-fprintf("\tDensity: %f Kg/m^3\n", rho);
+fprintf('\tDensity: %f Kg/m^3\n', rho);
+set(gas,'T', T, 'P', P, 'X', gascomp);
+lambda = thermalConductivity(gas);
+fprintf('\tThermalConductivity: %f W/(mK)\n', lambda);
 cp = cp_mass(gas);
-fprintf("\tSpecific Heat: %f J/(Kg K)\n", cp);
-
+fprintf('\tSpecific Heat: %f J/(Kg K)\n', cp);
 alpha = lambda / (rho * cp);
 S = 0.3;
-fprintf("\tAssume flame speed to be: %f m/s\n", S);
+fprintf('\tAssume flame speed to be: %f m/s\n', S);
 delta = alpha / S;
-fprintf("\tEstimated flame thickness: %f mm\n", delta*1000);
+fprintf('\tEstimated flame thickness: %f mm\n', delta*1000);
 
-fprintf("===============Hydrogen-Oxygen===============\n");
+fprintf('===============Hydrogen-Oxygen===============\n');
 gas = GRI30('Mix');
 gascomp = 'H2:2, O2:1';
-set(gas,'T', T, 'P', P, 'X', gascomp);
-
-lambda = thermalConductivity(gas);
-fprintf("\tThermalConductivity: %f W/(mK)\n", lambda);
+set(gas,'T', Tu, 'P', P, 'X', gascomp);
 rho = density(gas);
-fprintf("\tDensity: %f Kg/m^3\n", rho);
+fprintf('\tDensity: %f Kg/m^3\n', rho);
+set(gas,'T', T, 'P', P, 'X', gascomp);
+lambda = thermalConductivity(gas);
+fprintf('\tThermalConductivity: %f W/(mK)\n', lambda);
 cp = cp_mass(gas);
-fprintf("\tSpecific Heat: %f J/(Kg K)\n", cp);
-
+fprintf('\tSpecific Heat: %f J/(Kg K)\n', cp);
 alpha = lambda / (rho * cp);
 S = 3;
-fprintf("\tAssume flame speed to be: %f m/s\n", S);
+fprintf('\tAssume flame speed to be: %f m/s\n', S);
 delta = alpha / S;
-fprintf("\tEstimated flame thickness: %f mm\n", delta*1000);
+fprintf('\tEstimated flame thickness: %f mm\n', delta*1000);
 
-fprintf("=============================================\n");
+fprintf('=============================================\n');
