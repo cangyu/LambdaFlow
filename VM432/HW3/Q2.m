67851a8b928fa960a2710cabc3cae4052839a7c5
Yu Cang <yhcy1993@gmail.com>
2018-11-15 09:31:40 +0800

    fix choice of thermal conductivity and specific heat, integral of source term still not correct

--- VM432/HW3/Q2.m
+++ VM432/HW3/Q2.m
@@ -1,7 +1,7 @@
 clear all; close all; clc;
 
 T0 = 300;
-Ti = 2000;
+Ti = 2200;
 Tf = 2300;
 P = oneatm;
 
@@ -10,9 +10,17 @@ gascomp = 'CH4:0.5, O2:1, N2:3.6';
 
 set(gas,'T', T0, 'P', P, 'X', gascomp);
 rho_u = density(gas);
-lambda = thermalConductivity(gas);
-cp = cp_mass(gas);
-SL = sqrt(2*lambda*summation(gas, Ti, Tf, 10000))/(rho_u * cp * (Ti - T0));
+lambda1 = thermalConductivity(gas);
+cp1 = cp_mass(gas);
+
+set(gas,'T', Tf, 'P', P, 'X', gascomp);
+lambda2 = thermalConductivity(gas);
+cp2 = cp_mass(gas);
+
+lambda = (lambda1 + lambda2)/2;
+cp = (cp1 + cp2)/2;
+
+SL = sqrt(2*lambda*summation(gas, Ti, Tf))/(rho_u * cp * (Ti - T0));
 
 fprintf('\tDensity: %f Kg/m^3\n', rho_u);
 fprintf('\tThermal Conductivity: %f W/(m K)\n', lambda);
@@ -26,13 +34,14 @@ function ret = s(tp, temp)
     ret = dot(w, h) * temp * gasconstant;
 end
 
-function ret = summation(tp, Ta, Tb, N)
+function ret = summation(tp, Ta, Tb)
+    N = 10000;
     step = (Tb-Ta)/N;
-    cT = Ta - step/2;
+    cur_T = Ta - step/2;
     ret = 0;
     for i = 1:N
-        cT = cT + step;
-        ret = ret + s(tp, cT);
+        cur_T = cur_T + step;
+        ret = ret + s(tp, cur_T);
     end
     ret = ret * step;    
 end
