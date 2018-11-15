bb0495dac0613e0270344d6fa51e51d4dbcd5917
Yu Cang <yhcy1993@gmail.com>
2018-11-15 09:39:53 +0800

    fix lambda and cp, integral of source term still not correct

--- VM432/HW3/Q2.m
+++ VM432/HW3/Q2.m
@@ -10,8 +10,16 @@ gascomp = 'CH4:0.5, O2:1, N2:3.6';
 
 set(gas,'T', T0, 'P', P, 'X', gascomp);
 rho_u = density(gas);
-lambda = thermalConductivity(gas);
-cp = cp_mass(gas);
+lambda1 = thermalConductivity(gas);
+cp1 = cp_mass(gas);
+
+set(gas,'T', Tf, 'P', P, 'X', gascomp);
+lambda2 = thermalConductivity(gas);
+cp2 = cp_mass(gas);
+
+lambda = (lambda1 + lambda2)/2;
+cp = (cp1 + cp2)/2;
+
 SL = sqrt(2*lambda*summation(gas, Ti, Tf, 10000))/(rho_u * cp * (Ti - T0));
 
 fprintf('\tDensity: %f Kg/m^3\n', rho_u);
